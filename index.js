42352d09b88582519ca3f874577281a0058f87f7
Pawan Shrestha <pawanshrestha5758@gmail.com>
2024-01-28 23:11:41 +0545

    node practice

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require("http");
 const greeting = require("./library");
+const mathOperation = require("./math ");
+const uppercase = require("to-upper");
 
 http
   .createServer((req, res) => {
     greeting.getHello();
+    const result = uppercase("Hello pawan");
+    mathOperation.multiplication(`${2}${3}`);
     greeting.getNamaste("pawan");
     res.writeHead(200, { "content-type": "text/html" });
     test.write("hello world");
---
+++ math .js
@@ -0,0 +1,7 @@
+const sum = (a, b) => {
+  return a + b;
+};
+const multiplication = (a, b) => {
+  return a * b;
+};
+module.exports = { sum, multiplication };
